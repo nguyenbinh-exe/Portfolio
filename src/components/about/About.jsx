daf759406d6cf5a9486d94c7a7ad24af6939b0f2
nguyenbinh-exe <nguyen.binhnl212@gmail.com>
2023-06-03 13:20:53 -0500

    Added About section
    - Need new image

---
+++ src/components/about/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./about.css";
+import ProfileImg from "../../assets/profile.jpg";
+import Resume from "../../assets/resume05-2023.pdf"
+import Info from "./Info";
+
+const About = () => {
+  return (
+      <section className="about section" id="about">
+          <h2 className="section_title">About Me</h2>
+          <span className="section_subtitle">I am a First Generation Vietnamese-American College Student.</span>
+
+          <div className="about_container container grid">
+              <img src={ProfileImg} alt="" className="about_img" />
+              <div className="about_data">
+                <Info />
+                  <p className="about_description"></p>
+                  <a download="" href={Resume} className="button button--flex">Download Resume</a>
+              </div>
+          </div>
+      </section>
+  )
+}
+
+export default About
---
+++ src/components/about/Info.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Info = () => {
+  return (
+      <div className="about_info grid">
+          <div className="about_box">
+              <h3 className="about_title">Education <i className="uil uil-graduation-cap"></i></h3>
+              <span className="about_subtitle">Expected Graduation in December 2023</span>
+          </div>
+          <div className="about_box">
+              <h3 className="about_title">Experience <i className="uil uil-award-alt"></i></h3>
+              <span className="about_subtitle">Entry Level</span>
+          </div>
+          <div className="about_box">
+              <h3 className="about_title">Occupation <i className="uil uil-briefcase-alt"></i></h3>
+              <span className="about_subtitle">Student</span>
+          </div>
+      </div>
+  )
+}
+
+export default Info
